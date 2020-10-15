5ff10b63660002b8c554b9a07f78256fdfdf0c32
mateus.borja <mateus.borja@gmail.com>
2020-10-15 01:38:34 -0400

    new fun onClick

--- app/src/main/java/com/mateusborja/kotlindev1/MainActivity.kt
+++ app/src/main/java/com/mateusborja/kotlindev1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.mateusborja.kotlindev1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +13,8 @@ class MainActivity : AppCompatActivity() {
 
 
     fun botaoClique(view: View) {
-
-
+        var texto = findViewById(R.id.text1view1) as TextView
+        texto.setText("Eu vou conseguir!")
     }
 
 
